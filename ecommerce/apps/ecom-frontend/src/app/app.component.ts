6410b3a2d3598334da5e2e5c7036aea60ea9aecd
Pedro Stein <zodtewill@gmail.com>
2025-04-23 10:51:01 -0300

    chore: preconfigure Angular workspace (tsconfig.spec, Jasmine types, Nx CLI)

--- ecommerce/apps/ecom-frontend/src/app/app.component.ts
+++ ecommerce/apps/ecom-frontend/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { FontAwesomeModule, FaIconLibrary, FaConfig } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [FontAwesomeModule],
 })
 export class AppComponent implements OnInit {
-  // 1) Correto: injeta o serviço FaIconLibrary, anotando o tipo explicitamente
-  private faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+  private lib = inject(FaIconLibrary);
+  private cfg = inject(FaConfig);
 
   ngOnInit(): void {
-    // 2) Registra todos os ícones definidos no array
-    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+    this.cfg.defaultPrefix = 'fas';   // ✔ muda para sólido
+    this.lib.addIcons(faUser);        // registra o ícone
   }
 }
